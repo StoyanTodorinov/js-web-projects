1d86a5895b67c15340d4f2c36c0254cca14db9dd
Your Name <you@example.com>
2018-07-04 17:59:58 +0300

    structure changes

---
+++ api/rest/models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const encryption = require('../utilities/encryption')
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
+  lastName: { type: String, required: true },
+  salt: String,
+  hashedPass: String,
+  roles: [String]
+})
+
+userSchema.method({
+  authenticate: function (password) {
+    return encryption.generateHashedPassword(this.salt, password) === this.hashedPass
+  }
+})
+
+let User = mongoose.model('User', userSchema)
+
+module.exports = User
