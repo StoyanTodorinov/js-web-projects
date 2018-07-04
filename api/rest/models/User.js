8c6c59303c78e158ecb5fa33952c296e561ad807
Your Name <you@example.com>
2018-07-04 18:00:46 +0300

    user model

--- api/rest/models/User.js
+++ api/rest/models/User.js
@@ -4,10 +4,11 @@ const encryption = require('../utilities/encryption')
 let userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true },
-  lastName: { type: String, required: true },
+  name: { type: String, required: true },
   salt: String,
   hashedPass: String,
-  roles: [String]
+  roles: [String],
+  favorites: { type: [String], default: [] }
 })
 
 userSchema.method({
