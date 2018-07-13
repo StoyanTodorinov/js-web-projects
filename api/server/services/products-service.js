d6ac359dc28cf9088e80b9ea799c7c9629d66466
Your Name <you@example.com>
2018-07-13 18:56:38 +0300

    product service bug

--- api/server/services/products-service.js
+++ api/server/services/products-service.js
@@ -1,31 +1,31 @@
 const Product = require('mongoose').model('Product')
 
-async function createProduct(product) {
+async function createProduct (product) {
   let newProduct = await Product.create(product)
   return newProduct
 }
 
-async function getAllProductsByCategory(category) {
+async function getAllProductsByCategory (category) {
   let products = await Product.find({ categoryName: category })
   return products
 }
 
-async function getProductById(productId) {
+async function getProductById (productId) {
   let product = await Product.findById(productId)
   return product
 }
 
-async function getPromoProducts() {
+async function getPromoProducts () {
   let products = await Product.find({ promo: { $ne: 0 } }).sort('-promo')
   return products
 }
 
-async function getNewProducts() {
+async function getNewProducts () {
   let products = await Product.find({}).sort('-date').limit(3)
   return products
 }
 
-async function getAllProductsByArrayOfIds(array) {
+async function getAllProductsByArrayOfIds (array) {
   let products = await Product.find({ '_id': { $in: array } })
   return products
 }
